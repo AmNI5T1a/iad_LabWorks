f376367dd770cee1c84e50dbdcbecc8d8baa96d1
CC-AmNI5TIa <56353744+CC-AmNI5TIa@users.noreply.github.com>
2020-09-09 15:11:01 -0700

    first commit

---
+++ FirstLW/FirstTask/FeatureSpace.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IAD
+{
+    public class FeatureSpace
+    {
+
+        public struct Point
+        {
+            private double xAxis;
+            private double yAxis;
+
+            public void PasteComponents(double x, double y)
+            {
+                this.xAxis = x;
+                this.yAxis = y;
+            }
+            public void DisplayComponents(){
+                Console.WriteLine($"x: {xAxis} y:{yAxis}");
+            }
+
+        }
+
+        public Point TakePointFromInput(Point tempPoint , double xAxis , double yAxis)
+        {
+            tempPoint.PasteComponents(xAxis, yAxis);
+            return tempPoint;
+        }
+
+        public void DisplayPointOnScreen()
+        {
+            Point testpoint = new Point();
+            testpoint = TakePointFromInput(testpoint, 322, 228);
+            testpoint.DisplayComponents();
+        }
+    }
+}
---
+++ FirstLW/FirstTask/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace IAD
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            //CreatePointsInFeaturedScreen();
+            /*<summary>Creating 2 list with certain range and calculate it 
+                                        using Euclid's Algorithm</summary>*/
+            //DistanceInFeaturedSpace();
+
+
+
+            //<summary>------THIS IS TEST BLOCK------------- </summary>
+            Console.WriteLine("starting...");
+            FeatureSpace FS = new FeatureSpace();
+            FS.DisplayPointOnScreen();
+        }
+    }
+}
