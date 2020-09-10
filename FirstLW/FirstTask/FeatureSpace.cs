92af554fb5533e209ca5f573617b2329963eb057
CC-AmNI5TIa <56353744+CC-AmNI5TIa@users.noreply.github.com>
2020-09-10 05:01:24 -0700

    Finished working on first task

--- FirstLW/FirstTask/FeatureSpace.cs
+++ FirstLW/FirstTask/FeatureSpace.cs
@@ -5,16 +5,15 @@ namespace IAD
 {
     public class FeatureSpace
     {
-        double[,] Display = new double[1920, 1080];
         public struct Point
         {
-            private double xAxis;
-            private double yAxis;
+            public double xAxis;
+            public double yAxis;
 
             public void PasteComponents(double x, double y)
             {
-                this.xAxis = x;
-                this.yAxis = y;
+                xAxis = x;
+                yAxis = y;
             }
             public void DisplayComponents()
             {
@@ -22,27 +21,40 @@ namespace IAD
             }
         }
 
-        public Point TakePointFromInput(Point tempPoint, double xAxis, double yAxis)
+        public Point TakePointFromInput(double xAxis, double yAxis)
         {
+            Point tempPoint = new Point();
             tempPoint.PasteComponents(xAxis, yAxis);
             return tempPoint;
         }
 
-        public void CreateDisplay()
-        {
-            double counter = 0;
+        /*<summary>
+        Points{i , j}
 
-            for (int i = 0; i < 1920; i++)
-                for (int j = 0; j < 1080; j++)
-                {
-                    Display[i, j] = counter;
-                    counter++;
-                }
-        }
-        /*<summary>Calculate distance between 2 points on screen using Euclidean algorithm</summary>*/
-        public void CalculateDistance(Point tempFirstPoint , Point tempSecondPoint)
+        firstList [i,i+1,i+2,i+3...i+n]
+        secondList [i,i+1,i+2,i+3...i+n]
+
+        Sqrt((firstlist[i]-secondList[i])^2+(firstList[j]-secondList[j])^2) -> ToConsole
+        
+        </summary>*/
+        public void CalculateDistance(Int16 other, Point tempFirstPoint)
         {
-            Console.Write($"Distance: ");
+            List<double> firstList = new List<double>();
+            List<double> secondList = new List<double>();
+            Random rnd = new Random();
+
+            for (int i = 0; i < other; i++)
+            {
+                firstList.Add(rnd.Next(0, 99999999));
+                secondList.Add(rnd.Next(0, 99999999));
+            }
+            Console.WriteLine("Your answer is: " + Math.Sqrt(Math.Pow((firstList[(int)tempFirstPoint.xAxis] - secondList[(int)tempFirstPoint.xAxis]), 2)
+                                                                + (Math.Pow((firstList[(int)tempFirstPoint.yAxis] - secondList[(int)tempFirstPoint.yAxis]), 2))));
+            Console.WriteLine("-----Options for Debugging-----");
+            Console.WriteLine($"Sqrt(({(firstList[(int)tempFirstPoint.xAxis]).ToString()} - {(secondList[(int)tempFirstPoint.xAxis]).ToString()})^2"
+                                        + $"({(firstList[(int)tempFirstPoint.yAxis]).ToString()} - {(secondList[(int)tempFirstPoint.yAxis]).ToString()})^2)");
+
+            Console.WriteLine($"First point: {tempFirstPoint.xAxis.ToString()}\nSecond point: {tempFirstPoint.yAxis.ToString()}");
         }
     }
 }
--- FirstLW/FirstTask/MainMenu.cs
+++ FirstLW/FirstTask/MainMenu.cs
@@ -7,28 +7,28 @@ namespace IAD
     {
         static void Main()
         {
-
-            //CreatePointsInFeaturedScreen();
-            /*<summary>Creating 2 list with certain range and calculate it 
-                                        using Euclid's Algorithm</summary>*/
-            //DistanceInFeaturedSpace();
-
-
-
-            //<summary>------THIS IS TEST BLOCK------------- </summary>
-
-
-            //intialize 2 points
-            //create 2 lists
-            //start distanceBetweenPoints func
             #region Intializate Components
-            FeatureSpace.Point firstPoint = new FeatureSpace.Point();
-            FeatureSpace.Point secondPoint = new FeatureSpace.Point();
+            FeatureSpace.Point Points = new FeatureSpace.Point();
             FeatureSpace FS = new FeatureSpace();
+            Console.WriteLine("Paste first point value:");
+            int firstPoint = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Paste second point value:");
+            int secondPoint = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Paste List length value:");
+            short listLength = Convert.ToInt16(Console.ReadLine());
+            if (listLength <= firstPoint || listLength <= secondPoint)
+            {
+                Console.WriteLine("List length can't be less than point we will use");
+                while(listLength <= firstPoint || listLength <= secondPoint){
+                    Console.Write("Paste list length again: ");
+                    listLength = Convert.ToInt16(Console.ReadLine());
+                }
+            }
             #endregion
-            
-            //FS.TakePointFromInput(firstPoint,);
 
+            Points = FS.TakePointFromInput(firstPoint, secondPoint);
+
+            FS.CalculateDistance(listLength, Points);
         }
     }
 }
