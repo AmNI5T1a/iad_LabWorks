fbc4be54d5f099dc5efbf1c6a2ae632916737f51
CC-AmNI5TIa <56353744+CC-AmNI5TIa@users.noreply.github.com>
2020-09-21 13:00:46 -0700

    Deleted test block

---
+++ FirstLW/Scripts/InformativenessOfSpace.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IAD
+{
+    public class InformativenessOfSpace
+    {
+        /// <summary>
+        /// Informativeness can be characterized 
+        /// by the probability of detecting a specific object based on this feature
+        /// </summary>
+
+        
+    }
+}
